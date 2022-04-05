99c617430f910ef0652e3c81003acc357e69e935
tanimtonni12 <17103062iubat@gmail.com>
2022-04-05 17:28:17 +0600

    about us updated and 404 page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h3>about us</h3>
+            <h3 className='text-4xl text-red-800 font-bold mt-10'>About Us</h3>
+            <p className='text-xl w-2/4 text-justify mx-auto my-10'>Mac Studio is an entirely new Mac desktop. It packs outrageous performance, extensive connectivity, and new capabilities into an unbelievably compact form, putting everything you need within easy reach and transforming any space into a studio. And it all starts with your choice of the ferociously fast M1 Max or the all-new M1 Ultra — the most powerful chip ever in a personal computer.</p>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import img from '../../images/notfound.jpg'
 const NotFound = () => {
     return (
         <div className='mt-20'>
-            <h2 className='text-red-800 font-bold text-5xl'>Opps!!</h2>
+
             <img className='mx-auto' src={img} alt="" />
         </div>
     );
